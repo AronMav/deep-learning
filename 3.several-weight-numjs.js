af7f96f726cf42a1e054333bc7078625c835c0e6
Andrei Merzliakov <codymaverick@bk.ru>
2022-01-30 23:02:23 +0300

    Create 3.several-weight-numjs.js

---
+++ 3.several-weight-numjs.js
@@ -0,0 +1,15 @@
+var nj = require('numjs');
+
+let weights = nj.array([0.1, 0.2, 0]) 
+
+pred = (input, weights) =>{
+    return input.dot(weights).get(0)
+}
+
+toes = nj.array([8.5, 9.5, 9.9, 9.0])
+wlrec = nj.array([0.65, 0.8, 0.8, 0.9])
+nfans = nj.array([1.2, 1.3, 0.5, 1.0])
+
+input = nj.array([toes.get(0), wlrec.get(0), nfans.get(0)])
+
+console.log(pred(input, weights))

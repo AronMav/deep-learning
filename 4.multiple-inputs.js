b3c1e979fbab3a28579f867d59f9cd8066b5c83a
Andrei Merzliakov <codymaverick@bk.ru>
2022-01-30 23:39:30 +0300

    Create 4.multiple-inputs

---
+++ 4.multiple-inputs.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+
+weights = [0.3, 0.2, 0.9]
+
+pred = (input, weights) => {
+    return ele_mul(input, weights)
+}
+
+ele_mul = (number, vector) =>{
+    let output = [0,0,0]
+    assert.equal(output.length, vector.length)
+
+    for (let i = 0; i < vector.length; i++) {
+        output[i] = number * vector[i]
+    }
+    return output
+}
+
+wlrec = [0.65, 0.8, 0.8, 0.9]
+input = wlrec[0]
+
+console.log(pred(input, weights))

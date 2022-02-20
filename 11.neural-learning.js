04a882c9cb226fd074dac27d20f912fadff3dac3
Andrei Merzliakov <codymaverick@bk.ru>
2022-02-20 21:51:41 +0300

    Create 11.neural-learning.js

---
+++ 11.neural-learning.js
@@ -0,0 +1,18 @@
+// 4) COMPARE: Making A Prediction With a *Lower* Weight And Evaluating Error
+
+let weight = 0.1 
+
+production = (input, weight) => {
+    return input * weight
+}
+
+let number_of_toes = [8.5]
+let win_or_lose_binary = [1] // (won!!!)
+
+let input = number_of_toes[0]
+let true_ = win_or_lose_binary[0]
+
+let lr = 0.01
+let p_dn = production(input, weight-lr)
+let e_dn = (p_dn - true_) ** 2
+console.log(e_dn)

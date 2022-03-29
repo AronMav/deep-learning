269852d78450d427224be30c7af6179b4211d53d
Andrei Merzliakov <codymaverick@bk.ru>
2022-03-29 22:27:40 +0300

    Add batch gradient descent

--- python/18. MNIST.py
+++ python/18. MNIST.py
@@ -22,8 +22,9 @@ def relu(x): return (x >= 0) * x  # returns x if x > 0, return 0 otherwise
 def relu2deriv(x): return x >= 0  # returns 1 for input > 0, return 0 otherwise
 
 
-alpha, iterations, hidden_size, pixels_per_image, num_labels = (
-    0.005, 350, 40, 784, 10)
+batch_size = 100
+alpha, iterations = (0.001, 300)
+pixels_per_image, num_labels, hidden_size = (784, 10, 100)
 
 weights_0_1 = 0.2 * np.random.random((pixels_per_image, hidden_size)) - 0.1
 weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
@@ -31,8 +32,10 @@ weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
 for j in range(iterations):
     error, correct_accuracy = (0.0, 0)
 
-    for i in range(len(images)):
-        layer_0 = images[i:i + 1]
+    for i in range(int(len(images) / batch_size)):
+        batch_start, batch_end = ((i * batch_size), ((i + 1) * batch_size))
+
+        layer_0 = images[batch_start:batch_end]
         layer_1 = relu(np.dot(layer_0, weights_0_1))
 
         dropout_mask = np.random.randint(2, size=layer_1.shape)
@@ -40,18 +43,17 @@ for j in range(iterations):
 
         layer_2 = np.dot(layer_1, weights_1_2)
 
-        error += np.sum((labels[i:i + 1] - layer_2) ** 2)
-        correct_accuracy += int(np.argmax(layer_2) ==
-                                np.argmax(labels[i:i + 1]))
+        error += np.sum((labels[batch_start:batch_end] - layer_2) ** 2)
+        for k in range(batch_size):
+            correct_accuracy += int(np.argmax(layer_2[k:k+1]) ==
+                                    np.argmax(labels[batch_start+k:batch_start+k+1]))
 
-        layer_2_delta = (labels[i:i+1] - layer_2)
-        layer_1_delta = layer_2_delta.dot(weights_1_2.T)\
-            * relu2deriv(layer_1)
+            layer_2_delta = (labels[batch_start:batch_end]-layer_2)/batch_size
+            layer_1_delta = layer_2_delta.dot(weights_1_2.T)* relu2deriv(layer_1)
+            layer_1_delta *= dropout_mask
 
-        layer_1_delta *= dropout_mask
-
-        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+            weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+            weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
 
     sys.stdout.write("\n I:" + str(j) +
                      " Train-Err:" + str(error / float(len(images)))[0:5] +
